5d8825f7c4e7d93dc1a2e39b75fe36e0f487ffce
claytonweaver <cweaver@cbctrenton.com>
2019-11-28 23:31:05 -0500

    first upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+   
+    Console.WriteLine("What type of vacation would you like go on? Musical, tropical, or adventurous?");
+    
+    var vacationType = Console.ReadLine();
+    
+    var destination = "";
+    var transportType = "";
+    var groupSize = 0;
+    var result = "";
+
+    if (vacationType == "musical") {
+      destination = "New Orleans";
+    }
+    else if (vacationType == "adventurous") {
+      destination = "white water rafting in the Grand Canyon";
+    }
+    else if (vacationType == "tropical") {
+      destination = "beach vacation in Mexico";
+    }
+    else {
+      Console.WriteLine("Try a different answer.");
+    }
+   
+    
+    Console.WriteLine("How many are in your group?");
+    
+    groupSize = Convert.ToInt32(Console.ReadLine());
+
+    if (groupSize <= 2 && groupSize > 0) {
+      transportType = "first class flight";
+    }
+    else if (groupSize > 2 && groupSize < 6) {
+      transportType = "helicopter";
+    }
+    else if (groupSize >= 6) {
+      transportType = "charter flight";
+    }
+    else {
+      Console.WriteLine("Try a different number.");
+    }
+
+    if (vacationType == "adventurous") {  
+    result = $"Since you’re a group of {groupSize} going on an {vacationType} vacation, you should take a {transportType} to {destination}.";
+    }
+    else {
+    result = $"Since you’re a group of {groupSize} going on a {vacationType} vacation, you should take a {transportType} to {destination}.";
+    }
+    Console.WriteLine(result);
+
+  }
+}
